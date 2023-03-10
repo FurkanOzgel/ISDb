4e44b358a73bcf44590b3cc57f3733ed5d447cca
Furkan Özgel <furkan.ozgel@hotmail.com>
2023-03-10 15:21:51 +0300

    Designed Top Page

---
+++ javascript/home_page.js
@@ -0,0 +1,47 @@
+var styleElem = document.head.appendChild(document.createElement("style"));
+
+// Hides overview when mouse is on iframe
+function myOverFunction() { 
+    document.getElementsByClassName('big-song-card-video-overview')[0].style.visibility = 'hidden';
+    styleElem.innerHTML = ".big-song-card-video-container:before {visibility: hidden;}";
+
+}
+
+// Shows overview when mouse is not on iframe
+function myOutFunction() {
+    document.getElementsByClassName('big-song-card-video-overview')[0].style.visibility = 'visible';
+    styleElem.innerHTML = ".big-song-card-video-container:before {visibility: visible;}";
+}
+
+let list = document.getElementById("side-list");
+
+
+
+// Fetches data from response.json file and render them as li.
+fetch('../demo-data/response.json')
+    .then((response) => response.json())
+    .then((json) => {
+        let list = document.getElementById("side-list");
+    
+        for(i = 0; i < 31; i++) {
+            if(json.is_popular[i]==1) {
+                let li = document.createElement("li");
+                li.innerText = json.name[i];
+                li.id = "popular-list-item"
+                li.innerHTML = `
+                <div class="side-li-container">
+                    <div class="side-li-inner-container">
+                        <img id="side-li-img" src="`+json.img_url[i]+`">
+                        <div class="side-li-info-container">
+                            <p class="side-li-name-info">`+json.name[i]+`<br>
+                                <span style="font-size: 15px; color:gray;">&nbsp;&nbsp;&nbsp;-`+json.artist[i]+`</span></p>
+                            <p class="side-li-rate-info">
+                                <i id="side-li-star-icon" class="material-icons">star</i>`+json.isdb_rate[i]+`</p> 
+                        </div>
+                    </div>
+                </div>`;
+                
+                list.appendChild(li);
+            }
+        }
+    });
---
+++ javascript/top_page.js
@@ -0,0 +1,48 @@
+fetch('../demo-data/response.json')
+    .then((response) => response.json())
+    .then((json) => {
+
+        const sorted_isdb_rate = Object.values(json.isdb_rate).sort().reverse()
+        var key_list = Object.keys(json.isdb_rate)
+        var sorted_id = []
+
+        sorted_isdb_rate.forEach(
+            (rate) => {
+                const id = key_list.find(key => json.isdb_rate[key] === rate)
+                const index = key_list.indexOf(id);
+                if (index > -1) { 
+                    key_list.splice(index, 1);
+                  }
+                sorted_id.push(id)
+            }
+        )
+
+        let list = document.getElementById("top-song-list");
+
+        var i = 0
+
+        sorted_id.forEach((id) => {
+
+            i = i + 1
+
+            let li = document.createElement("li");
+
+            li.className = "top-list-li";
+
+            li.innerHTML =`
+            <div class="top-list-item">
+                <div class="top-list-item-container">
+                    <img class="top-list-item-img" src="`+json.img_url[id]+`">
+                    <div class="side-li-info-container">
+                        <p class="top-list-item-header" >`+i+`. `+json.name[id]+`<br>
+                            <span style="font-size: 15px; color:gray;">&nbsp;&nbsp;&nbsp;-`+json.artist[id]+`</span></p>
+                        <p class="top-list-item-rate-info">
+                            <i class="material-icons side-li-star-icon">star</i>`+json.isdb_rate[id]+`</p> 
+                    </div>
+                </div>
+            </div>
+            `
+            list.appendChild(li);
+        })
+        
+    });
